5132780d92b2c9d9e4486ac3c3de84d6016b9243
Dz0nZ1 <dzonzi777@gmail.com>
2024-02-24 11:19:40 +0100

    Created RoleClaimsConfiguration

---
+++ Test/Test.Infrastructure/Domain/Identity/RoleClaimsConfiguration.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Test.Infrastructure.Domain.Identity;
+
+public class RoleClaimsConfiguration : IEntityTypeConfiguration<IdentityRoleClaim<string>>
+{
+    public void Configure(EntityTypeBuilder<IdentityRoleClaim<string>> builder)
+    {
+        builder.ToTable("RoleClaims");
+    }
+}
