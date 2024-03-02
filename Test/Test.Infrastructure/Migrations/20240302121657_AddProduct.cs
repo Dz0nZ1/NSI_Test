1ae6ab0463a09eba6f3162cad2d1ad1db1d96430
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-02 23:28:08 +0100

    Created new migrations to the project

---
+++ Test/Test.Infrastructure/Migrations/20240302121657_AddProduct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "34DE6D7C-4270-425B-987F-8D2CC41CD857",
+                column: "NormalizedName",
+                value: "STUDENTSERVICE");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "34DE6D7C-4270-425B-987F-8D2CC41CD857",
+                column: "NormalizedName",
+                value: "EMPLOYEE");
+        }
+    }
+}
