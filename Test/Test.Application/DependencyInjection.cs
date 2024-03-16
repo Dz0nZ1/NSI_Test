03e0af260d7d002ed6023b9bdd2af862c32addbe
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-16 18:48:34 +0100

    Added Mediator and FluentValidation services to AddApplication dependency

--- Test/Test.Application/DependencyInjection.cs
+++ Test/Test.Application/DependencyInjection.cs
@@ -1,17 +1,32 @@
+using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Test.Application.Common.Behaviours;
 
 namespace Test.Application;
 
 public static class DependencyInjection
 {
-
-
+    
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddControllersWithViews()
             .AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        services.AddFluentValidationAutoValidation();
+        services.AddFluentValidationClientsideAdapters();
+        
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        
         return services;
     }
     
