aebad245441efd1cadc814b8056efcebbd0e9152
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-22 03:37:29 +0100

    Updated project files

--- Test/Test.Api/obj/Debug/net8.0/Test.Api.AssemblyInfo.cs
+++ Test/Test.Api/obj/Debug/net8.0/Test.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc13703f8ea73e80013e0a6259d92482e8322355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f058e803d40197b85845e73b0f73f400299866f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
